6dbb6e301159f99d219a69f8d035d47ac77e0645
TGhoul1997 <114195586+TGhoul1997@users.noreply.github.com>
2024-05-21 00:40:49 -0500

    all done except for

--- script.js
+++ script.js
@@ -2,26 +2,95 @@ let nombreCompleto;
 do {
     nombreCompleto = prompt("Hola! Para accceder a un préstamo a cuotas ingrese su nombre completo o escriba 'ESC' para salir").toUpperCase();
     if (nombreCompleto !== "ESC") {
-        ingresarDni = prompt("Hola " + nombreCompleto + ". Digite su DNI");
-        ingresarMonto = parseFloat("Ingrese el monto en soles que desea solicitar");
-        cuotasRequeridas = parseInt(prompt("Ingresa el número de cuotas requeridas (cuotas: 3, 6, 12 y 36)"));
-        switch (cuotasRequeridas) {
-            case 3:
-                const operacionTresCuotas = (a, b) => (a*20) / b;
-                console.log(operacionTresCuotas(ingresarMonto, cuotasRequeridas));
-            case 6:
-                const operacionSeisCuotas = (a, b) => (a*15) / b;
-                console.log(operacionSeisCuotas(ingresarMonto, cuotasRequeridas));
-            case 12:
-                const operacionDoceCuotas = (a, b) => (a*10) / b;
-                console.log(operacionDoceCuotas(ingresarMonto, cuotasRequeridas));
-            case 36:
-                const operacionTresSeisCuotas = (a, b) => (a*5) / b;
-                console.log(operacionTresSeisCuotas(ingresarMonto, cuotasRequeridas));
-            default:
-                alert("El número de cuotas requeridas no es válida");
-                break;
-        }
+        do{
+            let ingresarDni = prompt("Hola " + nombreCompleto + ". Digite su DNI");
+            if (ingresarDni !== "" && ingresarDni.length === 8) {
+                let ingresarMonto = parseFloat(prompt("Ingrese el monto en soles que desea solicitar"));
+                if (ingresarMonto !== "" && ingresarMonto >= 1000) {
+                    let cuotasRequeridas = parseInt(prompt("Ingresa el número de cuotas requeridas (cuotas: 3, 6, 12 y 36)"));
+                    switch (cuotasRequeridas) {
+                        case 3:
+                            function operacionTresCuotas (a, b, c){
+                                switch (c) {
+                                    case "cuota":
+                                        return (a * 1.20) / b;
+                                        break;
+                                    case "total":
+                                        return a * 1.20;
+                                        break;
+                                    default:
+                                        return 0;
+                                        break;
+                                }
+                            }
+                            for(let i = 1; i <= 3; i++) {
+                                console.log("Cuota " + i + ":" + operacionTresCuotas (ingresarMonto, cuotasRequeridas, cuota));
+                                console.log("Monto Total: " + operacionTresCuotas (ingresarMonto, cuotasRequeridas, total));
+                            }
+                        case 6:
+                            function operacionSeisCuotas (a, b, c){
+                                switch (c) {
+                                    case "cuota":
+                                        return (a * 1.15) / b;
+                                        break;
+                                    case "total":
+                                        return a * 1.15;
+                                        break;
+                                    default:
+                                        return 0;
+                                        break;
+                                }
+                            }
+                            for(let i = 1; i <= 6; i++) {
+                                console.log("Cuota " + i + ":" + operacionSeisCuotas (ingresarMonto, cuotasRequeridas, cuota));
+                                console.log("Monto Total: " + operacionSeisCuotas (ingresarMonto, cuotasRequeridas, total));
+                            }
+                        case 12:
+                            function operacionDoceCuotas (a, b, c){
+                                switch (c) {
+                                    case "cuota":
+                                        return (a * 1.10) / b;
+                                        break;
+                                    case "total":
+                                        return a * 1.10;
+                                        break;
+                                    default:
+                                        return 0;
+                                        break;
+                                }
+                            }
+                            for(let i = 1; i <= 12; i++) {
+                                console.log("Cuota " + i + ":" + operacionDoceCuotas (ingresarMonto, cuotasRequeridas, cuota));
+                                console.log("Monto Total: " + operacionDoceCuotas (ingresarMonto, cuotasRequeridas, total));
+                            }
+                        case 36:
+                            function operacionTresSeisCuotas (a, b, c){
+                                switch (c) {
+                                    case "cuota":
+                                        return (a * 1.05) / b;
+                                        break;
+                                    case "total":
+                                        return a * 1.05;
+                                        break;
+                                    default:
+                                        return 0;
+                                        break;
+                                }
+                            }
+                            for(let i = 1; i <= 36; i++) {
+                                console.log("Cuota " + i + ":" + operacionTresSeisCuotas (ingresarMonto, cuotasRequeridas, cuota));
+                                console.log("Monto Total: " + operacionTresSeisCuotas (ingresarMonto, cuotasRequeridas, total));
+                            }
+                        default:
+                            alert("El número de cuotas requeridas no es válida");
+                            break;
+                    }
+                } else {
+                    alert("Debe ingresar un monto para poder continuar");
+                }
+            } else {
+                alert("Debe ingresar su DNI para poder continuar");
+            }
+        } while (ingresarDni !== "");
     }
 } while (nombreCompleto !== "ESC");
-// for para imprimir las cuotas por mes y su total con if por las constantes
